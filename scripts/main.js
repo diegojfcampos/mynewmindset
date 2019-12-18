a8db12e1f936952ae10b75b09c93761df9ea9fec
Diego J F Campos <diegojfcampos@gmail.com>
2019-12-18 20:46:45 -0300

    Add

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,46 @@
+var myImage = document.querySelector('img.desenvolvedor');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'img/diego j f campos.jpg') {
+      myImage.setAttribute ('src','img/diego j f campos1.jpg');
+    } else {
+      myImage.setAttribute ('src','img/diego j f campos.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'My New Mind Set world!';
+
+function setUserName() {
+    var myName = prompt('Por favor, digite seu nome.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+  
+
+/*var myHeading = document.querySelector('h1');
+myHeading.textContent = 'My New Mind Set world!'; 
+
+alert("Welcome at My New Mind Set");
+
+/*document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
 
 
+document.querySelector('img.github.png').onclick = function() {
+    alert('GitHub /diegojfcampos .');
+} */
 
-alert("Welcome at My New Mind Set")
+/*function multiply(num1,num2) {
+    var result = num1 * num2;
+    return result;
+  }
+
+multiply(50),(10);
+multiply(5),(10);
+multiply(200),(1000);
+*/
+
+
